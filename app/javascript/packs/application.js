c48eb2e067af4e8a192fb32ef7c5aa9747614cfa
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-03-02 01:43:37 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,7 @@
-// Entry point for the build script in your package.json
 import "../stylesheets/application.scss";
+
+const { setAuthHeaders } = require("apis/axios");
+const { initializeLogger } = require("common/logger");
+
+initializeLogger();
+setAuthHeaders();
