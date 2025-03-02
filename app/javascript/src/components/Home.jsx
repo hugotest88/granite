130df7048dec03fdf2b31d4e62432267cb9aee00
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-03-02 16:20:04 +0530

    Add neetoui

---
+++ app/javascript/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Button } from "neetoui";
+
+const Home = () => (
+  <div className="flex h-screen items-center justify-center">
+    <Button label="Hello World." />
+  </div>
+);
+
+export default Home;
